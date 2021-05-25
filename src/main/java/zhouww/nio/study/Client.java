4f86e3b3e72f3a2d73f0929097ed44b4770d714d
dell-PC\dell <zhouweiwei@iboxpay.com>
2021-05-25 18:58:14 +0800

    修改数据

---
+++ src/main/java/zhouww/nio/study/Client.java
@@ -0,0 +1,21 @@
+package zhouww.nio.study;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel=SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("localhost",8090));
+        ByteBuffer n=ByteBuffer.allocate(1024);
+        n.put("hskhfshfjshjfhjsdfhsdf".getBytes());
+        n.flip();   
+        socketChannel.write(n);
+        while (true){
+
+        }
+    }
+}
